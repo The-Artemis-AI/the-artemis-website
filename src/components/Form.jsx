64491ac8e17d260fb7a42b34e1ea3f84aa4de988
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2023-09-02 20:06:38 +0200

    commit 6a182e96cb8b5993f9cc311369921d5bcbb7e291.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,113 @@
-import React from 'react'
+import React, {useEffect,useState, useRef} from 'react'
+import Imager from "./Image.jsx"
+import ContactBg from "../assets/images/contact-bg.svg"
+import youtube from "../assets/images/youtube.svg"
 import message from "../assets/images/message.svg"
-
+import cancel from "../assets/images/xmark-solid.svg"
+import emailjs from "emailjs-com";
+import {ToastContainer, toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import {BrowserRouter} from "react-router-dom"
 import {HashLink as Link} from "react-router-hash-link"
 
 const Form = () => {
 
- 
+    const form = useRef()
+
+    const contactBtn = document.getElementById('secondPop')
+    const contactForm = document.getElementById('firstPop')
+  
+  
+    const [isContactFormVisible, setContactFormVisible] = useState(false);
+  
+    const handleContactPop2 = () => {
+      setContactFormVisible(!isContactFormVisible);
+    };
+    
+        
+
+    const sendEmail = (e)=>{
+        e.preventDefault();
+
+        emailjs.sendForm("service_hivarve", "template_db3vxyi", form.current, "15uvCbWBezx9UHs3k")
+        .then((result) => {
+            console.log(result.text)
+            if(result.text == "OK"){
+                toast.success("Message sent successfully",{
+                    theme:"dark",
+                    pauseOnHover:true,
+                });
+            }
+            else{
+                toast.error("Failed to send");
+            };
+        }, (error) => {
+            console.log(error.text)
+            toast.error("Failed to send");
+        });
+
+
+    }
+    
+    const checkToast = (e)=>{
+        e.preventDefault();
+        toast.success("Message sent successfully",{
+            theme:"dark",
+            pauseOnHover:true,
+        });
+    }
 
   return (
     <div>
+    <div className={`contactPop ${isContactFormVisible ? 'visibly': 'hidden'}`}>
+    <div className={`firstPop `} id='firstPop' style={{ backgroundImage:`url(${ContactBg})`}}>
+      <form ref={form} 
+      onSubmit={sendEmail}
+      >
+        <div className='input'>
+          <label> Name:</label>
+          <input type="text" placeholder="Enter your name"  name="name" />
+        </div>
+        <div className='input'>
+          <label> Email:</label>
+          <input type="email" placeholder="Enter your email" name="email" />
+        </div>
+        <div className='input'>
+          <label> Phone:</label>
+          <input type="text" placeholder="Enter your name" name="phone"/>
+        </div>
+        <div className='input'>
+          <label> Company:</label>
+          <input type="text" placeholder="Your company name" name="company"/>
+        </div>
+        <div className='input'>
+          <label> Inquiry:</label>
+          <select name="inquiry">
+            <option value="Help me build">Help me build</option>
+            <option value="Talk to the team">Talk to the team</option>
+            <option value="Apply for a job">Apply for a job</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div className='input'>
+          <label> Message:</label>
+          <textarea name="message"> </textarea>
+        </div>
+        <div className='input'>
+          <button type='submit' value="Send" >SEND</button>
+        </div>
+      </form>
+    </div>
     
+    
+  </div>
   <div className='secondPop'>
   <Link to="#ourContact" smooth>
       <button id='secondPop' >
-         <img src={message} alt="contact us"/>
+        {isContactFormVisible ? <img src={cancel} alt='cancel'/> : <img src={message} alt="contact us"/>}
       </button></Link>
     </div>
-    
+    <ToastContainer/>
   </div>
   )
 }
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Image({ImageSource, alt, style, ImageClass, onMouseOver, onMouseLeave, id, title}) {
+function Image({ImageSource, alt, style, ImageClass, onMouseOver, onMouseLeave, id, background}) {
   return (
     <div className="image" style={style} onMouseOver={onMouseOver} onMouseLeave={onMouseLeave} >
-    <img src={ImageSource} alt={alt} id={id} className={ImageClass} title={title}/>
+    <img src={ImageSource} alt={alt} id={id} className={ImageClass}/>
     </div>  
   )
   }
